b4e18680a0d04435cac3d58a9fc1411c15e9cc1f
Ethanscharlie <ethanscharlie@protonmail.com>
2024-11-02 10:39:49 -0600

    Enemy health and more rooms

--- src/creaters/Soldier_creater.cpp
+++ src/creaters/Soldier_creater.cpp
@@ -1,10 +1,15 @@
 #include "Soldier_creater.hpp"
 #include "Vector2f.hpp"
 #include "components/Soldier_component.hpp"
+#include "ldtk/LDTK_EntityData.hpp"
+#include <any>
 
-void createSoldier(Entity* entity) {
+void createSoldier(Entity *entity) {
   Sprite *sprite = entity->add<Sprite>();
   sprite->image = Image("res/images/evilBean.png");
 
-  entity->add<Soldier>();
+  int health = std::any_cast<int>(entity->get<LDTK::EntityData>()
+                                      ->instance->fieldInstances.at("Health")
+                                      .getValue());
+  entity->add<Soldier>()->health = health;
 }
